ff9c4482c86ebc00838837e2ee38d8a9cebb2a92
Dev Inoue <itisagoodcat@gmail.com>
2018-10-28 19:56:38 +0900

    Delete Artisan method.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,37 +11,5 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function artisan($cmd)
-    {
-        switch ($cmd) {
-            case 'dbmigrate': //マイグレーション
-                $exit = \Artisan::call('migrate', [
-                        '--force' => true,
-                    ]);
-                break;
-            case 'dbrollback': //ロールバック
-                $exit = \Artisan::call('migrate:rollback', [
-                    '--force' => true,
-                ]);
-                break;
-            case 'storage': //public/storage→storage/app/publicにシンボリックリンク作成
-                $exit = \Artisan::call('storage:link');
-                break;
-            case 'down': //メンテナンスモード有効(メンテナンスモードの適用対象に制限をかけないと復帰できなくなります)
-                $exit = \Artisan::call('down');
-                break;
-            case 'up': //メンテナンスモード無効
-                $exit = \Artisan::call('up');
-                break;
-            case 'cache': //キャッシュ全消去
-                $exit = \Artisan::call('cache:clear');
-                break;
-            default:
-                return view('welcome');
-                break;
-        }
-
-        return dd($exit);
-    }
 
 }
